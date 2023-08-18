8a2f3ba7c49839c3ecb786fbb8eb7e5018ad0546
mluu921 <mluu921@users.noreply.github.com>
2023-08-18 11:58:14 -0700

    initial commit

---
+++ app.R
@@ -0,0 +1,100 @@
+library(tidyverse)
+library(shiny)
+library(bslib)
+library(ggsankey)
+library(patchwork)
+
+data <- read_csv(here::here('data/u01_b35_data.csv'),
+                 show_col_types = FALSE)
+
+trt_options <- data |> pull(trt) |> unique() |> sort()
+
+ae_options <- data |>
+  count(ae, trt) |>
+  count(ae) |>
+  filter(n == 2) |> pull(ae)
+
+main <- layout_sidebar(
+  fillable = TRUE,
+  sidebar = sidebar(
+    selectizeInput(
+      'select_ae',
+      '1. Select Adverse Event',
+      choices = ae_options,
+      multiple = T,
+      options = list(placeholder = 'Select an Adverse Event')
+    ),
+    actionButton(
+      'btn_ae_visualize',
+      'Visualize',
+      icon = icon(name = 'chart-bar', lib = 'font-awesome')
+    ),
+    downloadButton(
+      'report',
+      'Download Report',
+      icon = icon(name = 'chart-bar', lib = 'font-awesome')
+    )
+    # actionButton(
+    #   'btn_download_r',
+    #   'Downloard R Results',
+    #   icon = icon(name = 'chart-bar', lib = 'font-awesome')
+    # )
+  ),
+  uiOutput('sankey_plots')
+)
+
+ui <- page_fillable(
+  main
+)
+
+shinyApp(ui, function(input, output) {
+  
+  plots <- eventReactive(input$btn_ae_visualize, {
+    
+    map(input$select_ae, \(x) {
+      
+      plot1 <- make_sankey_diagram(data, trt = trt_options[1], ae = x)
+      plot2 <- make_sankey_diagram(data, trt = trt_options[2], ae = x)
+      
+      plot1 / plot2
+      
+    }) |> set_names(nm = input$select_ae)
+    
+  })
+  
+
+  observeEvent(input$btn_ae_visualize, {
+    
+    req(plots())
+    
+    output$sankey_plots <- renderUI({
+      
+      pmap(list(plots(), 1:length(plots()), names(plots())), \(plot, i, title) {
+        
+        output[[paste0('plot', i)]] <- renderPlot({plot})
+        
+        tagList(h1(paste0(title)),
+                plotOutput(paste0('plot', i), height = '800px'),
+                br(),
+                hr())
+        
+      })
+    })
+    
+  })
+  
+  output$report <- downloadHandler(
+    filename = \(x) {paste0('pro_ctcae_ae_sankey_', Sys.Date(), '.pdf')},
+    content = function(file) {
+      
+      rmarkdown::render(
+        'explore_ae_template.Rmd',
+        output_file = file,
+        params = list('plots' = plots()),
+        envir = new.env(parent = globalenv())
+      )
+
+    }
+  )
+
+})
